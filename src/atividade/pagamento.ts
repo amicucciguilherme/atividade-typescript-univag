e3f4841cae01f1b467344fff84dcbcc2bce7e6cb
Guilherme Meamicucci <guilhermeamicucci@sedec.local>
2026-03-09 15:38:30 -0400

    agora vai com o codigo

---
+++ src/atividade/pagamento.ts
@@ -0,0 +1,30 @@
+// 1. Definimos um contrato (Interface)
+interface MetodoPagamento {
+    processar(valor: number): void;
+}
+
+// 2. Implementamos formas específicas de pagamento
+class PagamentoCartao implements MetodoPagamento {
+    processar(valor: number): void {
+        console.log(`Processando R$ ${valor} no Cartão de Crédito...`);
+    }
+}
+
+class PagamentoPix implements MetodoPagamento {
+    processar(valor: number): void {
+        console.log(`Gerando QR Code para Pix no valor de R$ ${valor}...`);
+    }
+}
+
+// 3. O Processador não precisa mudar se criarmos um novo método de pagamento
+class ProcessadorDeVendas {
+    executar(metodo: MetodoPagamento, valor: number) {
+        metodo.processar(valor);
+    }
+}
+
+// --- Testando o Código ---
+const processador = new ProcessadorDeVendas();
+const meuPix = new PagamentoPix();
+
+processador.executar(meuPix, 150.00);
